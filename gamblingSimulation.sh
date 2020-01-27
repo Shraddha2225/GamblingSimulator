828302b076c956b4e82cc83a6750ef681b940e26
Admin <shraddha>
2020-01-27 17:41:24 +0530

    ADDED:Calculated Total Amount Of Win/Loose for Tewnty Days

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -6,16 +6,46 @@ echo "Welcome Gambling Simulation"
 STAKE=100
 BET=1
 
+
+#calculate percentage
+Percentage=$(( STAKE * 50 / 100 ))
+
+uppercash=$(( STAKE + Percentage ))
+lowercash=$(( STAKE - Percentage ))
+cash=$STAKE
+
+#declare -A	DictWinLoose
+#function to check win or loose gambler
 function bet()
 {
-	result=$((RANDOM%2))
-	if [[ result -eq 1 ]]
+	while [[ $cash -lt uppercash && $cash -gt lowercash ]]
+	do
+		result=$((RANDOM%2))
+		if [[ result -eq 1 ]]
+		then
+			((cash++))
+		else
+			((cash--))
+		fi
+	done
+	PullAmount=$((cash - STAKE))
+	echo $PullAmount
+}
+
+#total amount  of win and loose
+function total_amount()
+{
+	for ((i=1; i<=20; i++))
+	do 
+			totalAmount=$((totalAmount + $(bet)))
+	done
+	if [[ $totalAmount -gt 0 ]]
 	then
-		echo "Win"
+		echo "Winner Count is : $totalAmount"
 	else
-		echo "Loose"
+		echo "Looser Count is : $totalAmount"
 	fi
 }
 
-#calling bet function here
-bet
+#calling to function
+total_amount
